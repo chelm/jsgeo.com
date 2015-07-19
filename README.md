82aa564f438e752f601b2a4f4a5a0247c24f0f97
chelm <christopher.helm@gmail.com>
2015-07-19 09:04:14 -0600

    adding a readme finally

---
+++ README.md
@@ -0,0 +1,33 @@
+# jsgeo.com 
+---- 
+
+This is the source for [jsgeo.com](http://jsgeo.com). It's a static site that get's built via [acetate.io](http://acetate.io). 
+
+To build this site there are few install steps: 
+
+```
+  git clone 
+  npm install 
+  npm run dev
+```
+
+Those steps run a series of npm tasks, most importantly is the build step: 
+
+```
+  # build the site into ./build
+  acetate build 
+
+  # or
+  
+  # build the site and open in a browser
+  acatate server --open  
+
+```
+
+## Want to help?
+
+I probably need more docs, but I'd love some help making the site really shine. Specifically these things: 
+
+* A better venue map...
+* Speakers need to show their Org/Affiliation and have links to thier online selves
+* Anything you want... seriously, the more fun and unique this site gets, the better. 
---
+++ src/main.js
@@ -0,0 +1,47 @@
+'use strict';
+let message = "Hey JS.Geo people";
+console.log(message);
+
+var bannerMap = d3Map("#banner", -75.1667, 39.9500, window.innerWidth, 425);
+
+/*var width = window.innerWidth,
+    height = 425;
+
+var tiler = d3.geo.tile()
+    .size([width, height]);
+
+var projection = d3.geo.mercator()
+    .center([-75.1667, 39.9500])
+    .scale((1 << 21) / 2 / Math.PI)
+    .translate([width / 2, height / 2]);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var svg = d3.select("#banner").append("svg")
+    .attr("id", "philly-map")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.selectAll("g")
+  .data(tiler
+    .scale(projection.scale() * 2 * Math.PI)
+    .translate(projection([0, 0])))
+  .enter().append("g")
+    .each(function(d) {
+      var g = d3.select(this);
+      var host = ["a", "b", "c"][(d[0] * 31 + d[1]) % 3];
+      var url = ["http://", host, ".tile.openstreetmap.us/vectiles-highroad/", d[2], "/", d[0], "/", d[1],".json"].join('');
+      d3.json(url, function(error, json) {
+        g.selectAll("path")
+            .data(json.features.sort(function(a, b) { return a.properties.sort_key - b.properties.sort_key; }))
+          .enter().append("path")
+            .attr("class", function(d) { return d.properties.kind; })
+            .attr("d", path);
+        });
+    });
+
+*/
+
+//39.956184, -75.191801 sceince center
+//39.955705, -75.182148 amtrak 
